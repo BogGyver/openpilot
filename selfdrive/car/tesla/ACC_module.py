a7929ffe9e8be296fb2b1c3aecafe55ae786a817
BogGyver <39277124+BogGyver@users.noreply.github.com>
2019-04-04 16:14:57 -0400

    controlling ACC via map data and speed limits same as Pedal

--- selfdrive/car/tesla/ACC_module.py
+++ selfdrive/car/tesla/ACC_module.py
@@ -257,7 +257,8 @@ class ACCController(object):
     # Relative velocity between the lead car and our set cruise speed.
     future_vrel_kph = lead_speed_kph - CS.v_cruise_actual
     # How much we can accelerate without exceeding the max allowed speed.
-    available_speed_kph = max_v_by_speed_limit(self.acc_speed_kph, speed_limit_kph, speed_limit_valid, set_speed_limit_active, speed_limit_offset,CS) - CS.v_cruise_actual
+    self.acc_speed_kph = max_v_by_speed_limit(self.acc_speed_kph, speed_limit_kph, speed_limit_valid, set_speed_limit_active, speed_limit_offset,CS)
+    available_speed_kph = self.acc_speed_kph - CS.v_cruise_actual
     half_press_kph, full_press_kph = self._get_cc_units_kph(CS.imperial_speed_units)
     # button to issue
     button = None
