14eef4f9d6e995ecb93150ef58429293c88ac0f3
BogGyver <39277124+BogGyver@users.noreply.github.com>
2019-08-19 23:06:26 -0400

    fix ALCA module

--- selfdrive/car/modules/ALCA_module.py
+++ selfdrive/car/modules/ALCA_module.py
@@ -282,24 +282,22 @@ class ALCAModelParser(object):
 
   def update(self, v_ego, md, r_poly, l_poly, r_prob, l_prob, lane_width):
 
-    self.ALCA_direction = cs.alcaDirection
-    self.ALCA_enabled = cs.alcaEnabled
-    self.ALCA_total_steps = cs.alcaTotalSteps
-    self.ALCA_error = self.ALCA_error or (cs.alcaError and not self.prev_CS_ALCA_error)
-    self.prev_CS_ALCA_error = cs.alcaError
+    for socket, _ in self.poller.poll(1):
+      if socket is self.alcaStatus:
+        self.alcas = tesla.ALCAStatus.from_bytes(socket.recv())
     
     if not self.ALCA_enabled:
       return np.array(r_poly),np.array(l_poly),r_prob, l_prob, lane_width
 
-        self.ALCA_direction = self.alcas.alcaDirection
-        self.ALCA_enabled = self.alcas.alcaEnabled
-        self.ALCA_total_steps = self.alcas.alcaTotalSteps
-        self.ALCA_error = self.ALCA_error or (self.alcas.alcaError and not self.prev_CS_ALCA_error)
-        self.prev_CS_ALCA_error = self.alcas.alcaError
-
     #if we don't have yet ALCA status, return same values
     if self.alcas is None:
       return np.array(r_poly),np.array(l_poly),r_prob, l_prob, lane_width
+     
+    self.ALCA_direction = self.alcas.alcaDirection
+    self.ALCA_enabled = self.alcas.alcaEnabled
+    self.ALCA_total_steps = self.alcas.alcaTotalSteps
+    self.ALCA_error = self.ALCA_error or (self.alcas.alcaError and not self.prev_CS_ALCA_error)
+    self.prev_CS_ALCA_error = self.alcas.alcaError
 
     #if error but no direction, the carcontroller component is fine and we need to reset
     if self.ALCA_error and (self.ALCA_direction == 0):
