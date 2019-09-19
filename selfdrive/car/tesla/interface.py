70416ad68155f03a1b0e3cdc1893ab2952ab9866
BogGyver <bogdan.butoi@gmail.com>
2019-09-19 08:02:01 -0400

    changed steering rate cost to 1.0 to reduce ping pong

--- selfdrive/car/tesla/interface.py
+++ selfdrive/car/tesla/interface.py
@@ -210,7 +210,7 @@ class CarInterface(object):
     ret.openpilotLongitudinalControl = True
     ret.steerLimitAlert = False
     ret.startAccel = 0.5
-    ret.steerRateCost = 0.7
+    ret.steerRateCost = 1.0 
     ret.radarOffCan = not CarSettings().get_value("useTeslaRadar")
 
     return ret
