baa67d2dcda950aea06a8a166ec1916c0e3d616d
BogGyver <bogdan.butoi@gmail.com>
2020-04-17 12:49:28 -0400

    fixing readconfig.sh for special characters

--- selfdrive/car/tesla/readconfig.sh
+++ selfdrive/car/tesla/readconfig.sh
@@ -1,3 +1,8 @@
 CFG_FILE=/data/bb_openpilot.cfg
-CFG_CONTENT=$(cat $CFG_FILE | sed -r '/[^=]+=[^=]+/!d' | sed -r 's/\s+=\s/=/g')
-eval "export $'CFG_CONTENT'"
+CFG_CONTENT=$(cat $CFG_FILE | sed -r "s/'/SINGLE_Q/" | sed -r '/[^=]+=[^=]+/!d' | sed -r 's/\s+=\s/=/g' | sed -e 's/[[:space:]]*\=[[:space:]]*/=/g' \
+        -e 's/#.*$//' \
+        -e 's/[[:space:]]*$//' \
+        -e 's/^[[:space:]]*//' \
+        -e "s/^\(.*\)=\([^\"']*\)$/\1=\"\2\"/" | sed -r "s/SINGLE_Q/'/" )
+eval "export $CFG_CONTENT"
+echo $CFG_CONTENT
