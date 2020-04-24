0022873d2117b891139a0f78ae93a55be542815a
Ubuntu <ubuntu@ip-172-31-27-219.ec2.internal>
2020-04-24 01:43:06 +0000

    Fix a flake8 presubmit

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -540,7 +540,7 @@ def manager_thread():
         kill_managed_process("driverview")
 
     # check the status of all processes, did any of them die?
-    running_list = ["%s%s\u001b[0m" % ("\u001b[32m" if running[p].is_alive() else "\u001b[31m", p) for p in running]
+    # running_list = ["%s%s\u001b[0m" % ("\u001b[32m" if running[p].is_alive() else "\u001b[31m", p) for p in running]
     #cloudlog.debug(' '.join(running_list))
 
     # Exit main loop when uninstall is needed
