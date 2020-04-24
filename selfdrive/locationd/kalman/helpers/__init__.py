e7d035b79f9dd103580215b333a451675b79a324
David Abrahams <40281893+AllWashedOut@users.noreply.github.com>
2020-04-24 11:16:03 -0700

    Presubmit lint fixes (#157)
    * Laika import lint errors
    * Unused variable lint error
    * Fix flake8 presubmit by adding backport dep to Pipfile

--- selfdrive/locationd/kalman/helpers/__init__.py
+++ selfdrive/locationd/kalman/helpers/__init__.py
@@ -108,7 +108,7 @@ SAT_OBS = [ObservationKind.PSEUDORANGE_GPS,
 
 
 def run_car_ekf_offline(kf, observations_by_kind):
-  from laika.raw_gnss import GNSSMeasurement
+  from laika.laika.raw_gnss import GNSSMeasurement
   observations = []
   # create list of observations with element format: [kind, time, data]
   for kind in observations_by_kind:
