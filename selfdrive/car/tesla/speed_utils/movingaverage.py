4da60e75794d79153c7fca2b0f667ca70fe4d403
Comma Device <device@comma.ai>
2020-05-11 20:09:46 -0700

    Simplify moving average code

--- selfdrive/car/tesla/speed_utils/movingaverage.py
+++ selfdrive/car/tesla/speed_utils/movingaverage.py
@@ -1,49 +1,18 @@
+import queue
+
 class MovingAverage():
   def __init__(self, length):
-    self.position = 0
     self.length = length
-    self.sum = 0.
-    self.no_items = 0
-    self.values = [0.] * length
-
+    self.reset()
+    
   def reset(self):
-    self.position = 0
-    self.sum = 0.
-    self.no_items = 0
-    self.values = [0.] * self.length 
+    self.queue = queue.Queue(maxsize=self.length)
+    self.sum = 0
 
-  def add(self,element):
-    if self.no_items == self.length:
-      self.no_items -= 1
-      self.sum -= self.values[self.position]
-    self.values[self.position] = element
-    self.sum += self.values[self.position]
-    self.no_items += 1
-    self.position += 1
-    if self.sum == 0.:
-      #all empty so initialize
-      self.position = 0
-      self.sum = 0.
-      self.no_items = 0
-      return 0.
-    self.position = self.position % self.length
-    return self.sum/self.no_items
-
-  def dele(self):
-    if self.no_items == 0:
-      return 0.
-    if self.no_items > 0:
-      self.no_items -= 1
-      self.sum -= self.values[self.position]
-      self.values[self.position] = 0.
-      self.position -= 1
-      if self.position < 0:
-        self.position = self.length-1
-    if self.sum == 0. or self.no_items == 0.:
-      #all empty so initialize
-      self.position = 0
-      self.sum = 0.
-      self.no_items = 0
-      return 0.
-    self.position = self.position % self.length
-    return self.sum/self.no_items
+  def add(self, sample):
+    if self.queue.full():
+      oldest_sample = q.get_nowait()
+      self.sum -= oldest_sample
+    self.queue.put_nowait(sample)
+    self.sum += sample
+    return self.sum / self.queue.qsize()
