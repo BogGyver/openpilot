bcfca8d3c22269a14a5671b600610875f249ad0c
Raf <rafcabezas@gmail.com>
2020-11-20 09:59:11 -0500

    Disable deprecated alert for grey panda

--- selfdrive/car/car_helpers.py
+++ selfdrive/car/car_helpers.py
@@ -25,8 +25,8 @@ def get_startup_event(car_recognized, controller_available, hw_type):
     event = EventName.startupNoCar
   elif car_recognized and not controller_available:
     event = EventName.startupNoControl
-  elif hw_type == HwType.greyPanda:
-    event = EventName.startupGreyPanda
+  # elif hw_type == HwType.greyPanda:
+  #   event = EventName.startupGreyPanda
   return event
 
 
