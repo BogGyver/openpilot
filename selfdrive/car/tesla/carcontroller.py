51539cb719a047139ce4712073e0fd959524ca33
BogGyver <bogdan.butoi@gmail.com>
2021-03-24 19:30:50 -0400

    changing the way we send long control

--- selfdrive/car/tesla/carcontroller.py
+++ selfdrive/car/tesla/carcontroller.py
@@ -75,6 +75,18 @@ class CarController():
       if self.CP.carFingerprint == CAR.AP1_MODELS:
         can_sends.append(self.tesla_can.create_ap1_long_control(self.v_target, tesla_accel_limits, tesla_jerk_limits, self.long_control_counter))
       self.long_control_counter = (self.long_control_counter + 1) % 8
+
+    if (not enabled) and self.CP.openpilotLongitudinalControl and (frame %2 == 0):
+      #send this values so we can enable at 0 km/h
+      v_target = 350
+      tesla_accel_limits = [-1.4000000000000004,1.8000000000000007]
+      tesla_jerk_limits = [-0.46000000000000085,0.47600000000000003]
+      if self.CP.carFingerprint == CAR.AP2_MODELS:
+        can_sends.append(self.tesla_can.create_ap2_long_control(self.v_target, tesla_accel_limits, tesla_jerk_limits, self.long_control_counter))
+      if self.CP.carFingerprint == CAR.AP1_MODELS:
+        can_sends.append(self.tesla_can.create_ap1_long_control(self.v_target, tesla_accel_limits, tesla_jerk_limits, self.long_control_counter))
+      self.long_control_counter = (self.long_control_counter + 1) % 8
+
     
     if not enabled:
       self.v_target = CS.out.vEgo
