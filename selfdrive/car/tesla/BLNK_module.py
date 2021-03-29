61f72aac600ab9b081ae726c4567c4da46731cd9
BogGyver <bogdan.butoi@gmail.com>
2021-03-29 00:57:28 -0400

    adding BLNK logic

--- selfdrive/car/tesla/BLNK_module.py
+++ selfdrive/car/tesla/BLNK_module.py
@@ -15,21 +15,11 @@ class Blinker:
             if self.override_frame_end == 0:
                 self.blinker_on_frame_start = frame
         elif (
-            CS.turn_signal_stalk_state == 0 and CS.prev_turn_signal_stalk_state > 0
+            CS.turn_signal_stalk_state == 0 and self.prev_turn_signal_stalk_state > 0
         ):  # turn signal stalk just turned off
             if frame - self.blinker_on_frame_start <= self.tap_duration_frames:
-                self.tap_direction = CS.prev_turn_signal_stalk_state
-                if CS.tapBlinkerExtension > 0 and self.override_frame_end == 0:
-                    blink_duration_frames = 58  # one blink takes ~580ms
-                    total_blinks = (
-                        3 + CS.tapBlinkerExtension
-                    )  # 3 blinks are minimum and controlled by the car
-                    self.override_frame_end = (
-                        self.blinker_on_frame_start
-                        + blink_duration_frames * total_blinks
-                    )
-                    self.override_direction = CS.prev_turn_signal_stalk_state
-
+                self.tap_direction = self.prev_turn_signal_stalk_state
+                
         if 0 < self.override_frame_end < frame or (
             self.override_direction > 0
             and (CS.turn_signal_stalk_state > 0 or self.override_frame_end == 0)
