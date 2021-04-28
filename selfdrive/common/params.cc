a9c57373032c1d9a758f9fe6cec3f7d15d737c13
BogGyver <bogdan.butoi@gmail.com>
2021-04-28 11:40:02 -0400

    pulling in the latest from Comma

--- selfdrive/common/params.cc
+++ selfdrive/common/params.cc
@@ -31,13 +31,9 @@ namespace {
 
 #if defined(QCOM) || defined(QCOM2)
 const std::string default_params_path = "/data/params";
-#else
-const std::string default_params_path = util::getenv_default("HOME", "/.comma/params", "/data/params");
-#endif
-
-#if defined(QCOM) || defined(QCOM2)
 const std::string persistent_params_path = "/persist/comma/params";
 #else
+const std::string default_params_path = util::getenv_default("HOME", "/.comma/params", "/data/params");
 const std::string persistent_params_path = default_params_path;
 #endif
 
