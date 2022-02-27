e3b20032666c48889b81243ac22b6c10c3f7038e
BogGyver <bogdan.butoi@gmail.com>
2022-02-27 18:09:49 -0500

    WIP UI

--- selfdrive/car/modules/qt/tsettings.hpp
+++ selfdrive/car/modules/qt/tsettings.hpp
@@ -6,7 +6,20 @@
 #include <QPushButton>
 #include <QVBoxLayout>
 
-
+#ifndef QCOM
+#include "selfdrive/ui/qt/offroad/networking.h"
+#endif
+#include "selfdrive/ui/qt/offroad/settings.h"
+#include "selfdrive/ui/qt/widgets/input.h"
+#include "selfdrive/ui/qt/widgets/toggle.h"
+#include "selfdrive/ui/qt/widgets/offroad_alerts.h"
+#include "selfdrive/ui/qt/widgets/scrollview.h"
+#include "selfdrive/ui/qt/widgets/controls.h"
+#include "selfdrive/ui/qt/widgets/ssh_keys.h"
+#include "common/params.h"
+#include "common/util.h"
+#include "selfdrive/hardware/hw.h"
+#include "selfdrive/ui/qt/home.h"
 
 class TinklaTogglesPanel : public ListWidget {
   Q_OBJECT
