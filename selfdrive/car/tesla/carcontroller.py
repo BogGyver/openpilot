04675576af3185be471b5e71c9dce8dff4cb1989
BogGyver <bogdan.butoi@gmail.com>
2022-02-28 15:48:47 -0500

    fixing tesla 2

--- selfdrive/car/tesla/carcontroller.py
+++ selfdrive/car/tesla/carcontroller.py
@@ -38,7 +38,7 @@ class CarController():
     
   
   #def update(self, c, enabled, CS, frame, actuators, cruise_cancel):
-  def update(self, enabled, CS, frame, actuators, cruise_cancel, pcm_speed, pcm_override, hud_alert, audible_alert,
+  def update(self, c, enabled, CS, frame, actuators, cruise_cancel, pcm_speed, pcm_override, hud_alert, audible_alert,
              left_line, right_line, lead, left_lane_depart, right_lane_depart): 
     if frame % 100 == 0:
       CS.useFollowModeAcc = load_bool_param("TinklaUseFollowACC",False)
