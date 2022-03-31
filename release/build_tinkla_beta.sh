2e91b400bc71fc972e1fd5a3eb32624c48ff2888
BogGyver <bogdan.butoi@gmail.com>
2022-03-31 09:12:29 -0400

    more on build and release scripts

--- release/build_tinkla_beta.sh
+++ release/build_tinkla_beta.sh
@@ -10,9 +10,7 @@ fi
 TINKLA_BETA_NUMBER="$1"
 
 SOURCE_DIR=/data/openpilot_dev
-TARGET_DIR=/data/openpilot_beta
-
-ln -sf $TARGET_DIR /data/pythonpath
+TARGET_DIR=/data/openpilot
 
 export GIT_COMMITTER_NAME="BogGyver"
 export GIT_COMMITTER_EMAIL="bogdan.butoi@gmail.com"
@@ -32,11 +30,13 @@ echo "[-] fetching public T=$SECONDS"
 cd $TARGET_DIR
 git prune || true
 git remote prune origin || true
+git fetch origin tesla_unity_dev
+git fetch origin tesla_unity_beta
 
-echo "[-] bringing devel in sync T=$SECONDS"
-#git checkout -f --track origin/tesla_unity_beta
+git checkout -f --track origin/tesla_unity_dev
+git reset --hard tesla_unity_dev
+git checkout tesla_unity_dev
 git reset --hard origin/tesla_unity_beta
-git checkout  tesla_unity_beta
 git clean -xdf
 
 
@@ -59,6 +59,7 @@ TINKLAVERSION=$(cat selfdrive/common/tinkla_version.h | awk -F[\"-]  '{print $2}
 echo "[-] copying files T=$SECONDS"
 cd $SOURCE_DIR
 cp -pR --parents $(cat $SOURCE_DIR/release/files_common) $TARGET_DIR/
+cp -pR --parents $(cat $SOURCE_DIR/release/files_tici) $TARGET_DIR/
 
 #update version files
 echo "#define COMMA_VERSION \"$VERSION-Beta$TINKLA_BETA_NUMBER\"" > $TARGET_DIR/selfdrive/common/version.h
