eb853b6ac68645d33883d93f5862df5499ceb54b
BogGyver <bogdan.butoi@gmail.com>
2022-04-01 11:19:11 -0400

    fixing (hopefully) auto-panda flash

--- selfdrive/pandad.py
+++ selfdrive/pandad.py
@@ -34,8 +34,7 @@ def flash_panda(panda_serial: str) -> Panda:
 
   if panda.bootstub or panda_signature != fw_signature:
     cloudlog.info("Panda firmware out of date, update required")
-    #panda.flash()
-    panda.recover() #BB trying to see if auto flash will work this way when the system boots
+    panda.flash()
     cloudlog.info("Done flashing")
 
   if panda.bootstub:
