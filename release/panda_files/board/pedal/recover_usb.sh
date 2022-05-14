16125e1f63524ff2463593ec4072377227152bea
BogGyver <bogdan.butoi@gmail.com>
2022-05-14 11:14:18 -0400

    pedal helper update

--- release/panda_files/board/pedal/recover_usb.sh
+++ release/panda_files/board/pedal/recover_usb.sh
@@ -2,6 +2,6 @@
 set -e
 
 DFU_UTIL="dfu-util"
-PYTHONPATH=/data/openpilot python3 enter_usb_dfu_mode.py
+PYTHONPATH=../../.. python3 enter_usb_dfu_mode.py
 $DFU_UTIL -d 0483:df11 -a 0 -s 0x08004000 -D ../obj/pedal.bin.signed
 $DFU_UTIL -d 0483:df11 -a 0 -s 0x08000000:leave -D ../obj/bootstub.pedal.bin
