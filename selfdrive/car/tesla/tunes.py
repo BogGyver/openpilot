53aafdfc4fc7dda4c88f3963732a3ca8f6c0fef7
BogGyver <bogdan.butoi@gmail.com>
2022-06-08 16:34:03 -0400

    WIP: pedal improvements

--- selfdrive/car/tesla/tunes.py
+++ selfdrive/car/tesla/tunes.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from enum import Enum
+from selfdrive.car.modules.CFG_module import load_bool_param
 
 
 class LongTunes(Enum):
@@ -12,12 +13,17 @@ class LongTunes(Enum):
 def set_long_tune(tune, name):
   # Improved longitudinal tune
   if name == LongTunes.PEDAL:
-    tune.kpBP = [0., 5., 35.]
-    tune.kpV = [0.50, 0.45, 0.45]
-    tune.kiBP = [0., 5., 35.]
-    tune.kiV = [0.01,0.01,0.01]
-
-
+    perf = load_bool_param("TinklaHasPerfMotor",False)
+    if not perf:
+      tune.kpBP = [0.0, 5.0, 22.0, 35.0]
+      tune.kiBP = [0.0, 5.0, 22.0, 35.0]
+      tune.kpV = [0.50, 0.45, 0.4, 0.4]
+      tune.kiV = [0.01, 0.01, 0.01, 0.01]
+    else:
+      tune.kpBP = [0.0, 5.0, 22.0, 35.0]
+      tune.kiBP = [0.0, 5.0, 22.0, 35.0]
+      tune.kpV = [0.3, 0.3, 0.35, 0.37]
+      tune.kiV = [0.07, 0.07, 0.093, 0.092]
   # Default longitudinal tune
   elif name == LongTunes.ACC:
     tune.kpBP = [0]
