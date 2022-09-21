ce17ba8cfa8008e19a48c585c12a78e70dd486b9
BogGyver <bogdan.butoi@gmail.com>
2022-09-21 01:00:04 -0400

    WIP adding string params

--- selfdrive/car/modules/qt/tinklaparams.h
+++ selfdrive/car/modules/qt/tinklaparams.h
@@ -6,6 +6,7 @@ using std::endl;
 #include <fstream>
 using std::ifstream;
 using std::ofstream;
+#include <sstream>
 #include <cstdlib> 
 #include "selfdrive/common/params.h"
 
@@ -65,6 +66,35 @@ float tinkla_get_float_param(const std::string &tinkla_param, float default_valu
 }
 
 void tinkla_set_float_param(const std::string &tinkla_param,float tinkla_param_value) {
+      ofstream ofile;
+      ofile.open(tinkla_params_path + "/" + tinkla_param);
+      if (ofile) {
+        ofile << tinkla_param_value;
+        ofile.close();
+      }
+}
+
+std::string tinkla_get_str_param(const std::string &tinkla_param, std::string default_value) {
+    ifstream ifile;
+    ifile.open(tinkla_params_path + "/" + tinkla_param);
+    if (!ifile) {
+      //no file assume default_value and create
+      ofstream ofile;
+      ofile.open(tinkla_params_path + "/" + tinkla_param);
+      if (ofile) {
+        ofile << default_value;
+        ofile.close();
+      }
+      return default_value;
+    } else {
+      std::stringstream strStream;
+      strStream << ifile.rdbuf();
+      std::string str = strStream.str();
+      return str;
+    }
+}
+
+void tinkla_set_str_param(const std::string &tinkla_param,std::string tinkla_param_value) {
       ofstream ofile;
       ofile.open(tinkla_params_path + "/" + tinkla_param);
       if (ofile) {
