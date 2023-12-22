c0fa6b41d7916f2b76c7f226a57f926d8ee958ff
BogGyver <39277124+BogGyver@users.noreply.github.com>
2023-12-22 08:50:11 -0500

    using the setting for numb period

--- selfdrive/car/modules/HSO_module.py
+++ selfdrive/car/modules/HSO_module.py
@@ -12,17 +12,17 @@ class HSOController:
             # if steering but not by ALCA
             if CS.HSOSteeringPressed:
                 self.frame_humanSteered = frame
-            elif (frame - self.frame_humanSteered < 50) and (CS.turnSignalStalkState > 0):  
+            elif (frame - self.frame_humanSteered < (CS.hsoNumbPeriod * 100)) and (CS.turnSignalStalkState > 0):  
                 # stalk locked, update frame
                 self.frame_humanSteered = frame
-            elif (frame - self.frame_humanSteered < 50):  
+            elif (frame - self.frame_humanSteered < (CS.hsoNumbPeriod * 100)):  
                 # Need more human testing of handoff timing
                 # Find steering difference between visiond model and human (no need to do every frame if we run out of CPU):
                 apply_steer = int(actuators.steeringAngleDeg)
                 angle_diff = abs(apply_steer - CS.out.steeringAngleDeg)
                 if angle_diff > 15.0:
                     self.frame_humanSteered = frame
-            if frame - self.frame_humanSteered < 50:
+            if frame - self.frame_humanSteered < (CS.hsoNumbPeriod * 100):
                 human_control = True
 
         self.human_control = human_control
