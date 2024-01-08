7b79a8dee4dfc880a77b73221f7d6940d1551f18
BogGyver <bogdan.butoi@gmail.com>
2024-01-08 13:55:55 -0500

    Tesla Unity v0.9.6-Beta33

--- common/tinkla_version.h
+++ common/tinkla_version.h
@@ -1,1 +1,1 @@
-#define TINKLA_VERSION "0.9.6-32"
+#define TINKLA_VERSION "0.9.6-33"
--- selfdrive/controls/lib/events.py
+++ selfdrive/controls/lib/events.py
@@ -445,7 +445,7 @@ EVENTS: Dict[int, Dict[str, Union[Alert, AlertCallbackType]]] = {
       "Steering Temporarily Unavailable",
       "",
       AlertStatus.userPrompt, AlertSize.small,
-      Priority.LOW, VisualAlert.steerRequired, AudibleAlert.prompt, 1.8),
+      Priority.LOW, VisualAlert.steerRequired, AudibleAlert.none, 1.8),
   },
 
   EventName.preDriverDistracted: {
